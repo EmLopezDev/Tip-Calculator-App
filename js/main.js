eeb939a1b094d2f68ae9d96f7c4d5f49010ff665
Emmanuel Lopez <emlopezdev@gmail.com>
2025-05-07 15:57:16 -0400

    switched radio inputs to buttons and created js file

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const billInput = document.getElementById("bill");
+const tipButtons = document.querySelectorAll(".app__card--tip-button");
+const tipCustom = document.getElementById("custom");
+const peopleInpuit = document.getElementById("number-of-people");
+const resetButton = document.getElementById("reset-button");
